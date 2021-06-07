9cf797e7356e84b853191a89f18fa14aa77ff1d0
Dave Stanke <davidstanke@gmail.com>
2021-06-07 20:22:49 +0000

    need suffix

--- misc/insert.sh
+++ misc/insert.sh
@@ -2,4 +2,4 @@
 
 bq --location=US load --source_format=NEWLINE_DELIMITED_JSON \
 --schema=log_sink.stderr_20210607.schema.json \
-log_sink.stderr sample_data.json   
+log_sink.stderr_20210607 sample_data.json   
